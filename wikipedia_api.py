ab0616f1383448e1fe4edbf027cb67b4ed0e67fc
MalinaCraciun23 <craciunmalina@yahoo.com>
2020-12-27 00:16:22 +0200

    created countries route

--- wikipedia_api.py
+++ wikipedia_api.py
@@ -5,6 +5,13 @@ from wikipedia_crawler import crawl_countries
 app = Flask(__name__)
 countries_col = MontyClient().db.countries
 
+
+@app.route("/toate-tarile")
+def get_countries():
+    countries = countries_col.find({}, {'_id': False})
+    return jsonify(list(countries))
+
+
 if __name__ == '__main__':
     crawl_countries()
     app.run(host="localhost", port=8000, debug=True)
